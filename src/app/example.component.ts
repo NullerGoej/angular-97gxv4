03d284875c8bd82ce5a30aed92aa42b1fbe51f40
NullerGoej <bastian2010@live.dk>
2023-03-10 10:46:06 +0100

    2 files modified

--- src/app/example.component.ts
+++ src/app/example.component.ts
@@ -11,6 +11,7 @@ export class ExampleComponent {
 
   message = 'To register press the button above!';
   name: string;
+  age: number;
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
@@ -18,12 +19,13 @@ export class ExampleComponent {
 
   confirm() {
     this.modal.dismiss(this.name, 'confirm');
+    this.modal.dismiss(this.age, 'confirm');
   }
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
+      this.message = `Hello, ${this.name}!`;
     }
   }
 }
